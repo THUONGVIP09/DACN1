002bf5256252a4ad1163f972c51223d6df4caf52
THUONGVIP09 <email@gmail.com>
2026-05-11 13:59:45 +0700

    thuongfix1

--- database.py
+++ database.py
@@ -1,12 +1,25 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Thay đổi connection string thành MySQL Docker của chúng ta
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@localhost:3307/traffic_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://user:password@localhost:3307/traffic_db"
+)
+
+print("DATABASE_URL =", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 def get_db():
